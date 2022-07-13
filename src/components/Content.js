b68c73989a8aa6f2833ad892b29bb17a8539e48b
Samuel Akinboro <victorakinboro2003@gmail.com>
2022-07-13 08:01:42 +0100

    comments

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,38 +1,46 @@
 import { Avatar } from '@material-ui/core'
-import React from 'react'
+import React, { useEffect, useState} from 'react'
 import styled from 'styled-components'
-import { ChatBubbleOutlineOutlined, Favorite, MoreHoriz, ReplyOutlined, ThumbUp, ThumbUpOutlined } from "@material-ui/icons"
+import { ChatBubbleOutlineOutlined, Favorite, MoreHoriz, ReplyOutlined, ThumbUp, ThumbUpOutlined, EmojiEmotionsOutlined, GifOutlined, FaceOutlined, SupervisedUserCircleOutlined } from "@material-ui/icons"
 
-function Content() {
+function Content({ userName, profilePicture, likes, liked, comments, text, postImage }) {
+    const [isLiked, setIsLiked] = useState(false);
+    const [allComments, setAllComments] = useState([]);
+
+    useEffect(()=>{
+        setIsLiked(liked)
+        setAllComments([...comments])
+    }, [])
+
     return (
         <Container>
             <Header>
                 <Profile>
-                    <Avatar src="https://images.unsplash.com/photo-1593990965215-075c1f918806?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8aGFuZHNvbWUlMjBibGFjayUyMG1hbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" />
+                    <Avatar src={profilePicture} />
                     <Title>
-                        <h4>Big Sam</h4>
+                        <h4>{userName}</h4>
                         <p>3d </p>
                     </Title>
                 </Profile>
                 <MoreHoriz />
             </Header>
             <Text>
-                <div>
-                The all New Apple Macbook Pro M1 chip is Here Guys
-                </div>
+                <div>{text}</div>
             </Text>
             <Image>
-                <img src="https://sb.nhattao.com/2018/12/11694856_macbook-pro-retina-15-inch-early-2015.jpg" alt="" />
+                {postImage && 
+                    <img src={postImage} alt="" />
+                }
             </Image>
             <EmojiCount>
                 <EmojiList>
                     <Emojis>
                         <ThumbUp className='like'/>
                         <Favorite className='love'/>
-                        <p>1M</p>
+                        <p>{likes}</p>
                     </Emojis>
                     <DetailInfo>
-                        <p>10k comments</p>
+                        <p>{allComments.length} comments</p>
                         <p>900 shares</p>
                     </DetailInfo>
                 </EmojiList>
@@ -53,6 +61,31 @@ function Content() {
                     </Icon>
                 </Reactios>
             </Bottom>
+            <CreateComment>
+                <Avatar src="https://images.unsplash.com/photo-1593990965215-075c1f918806?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8aGFuZHNvbWUlMjBibGFjayUyMG1hbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" className='avatar' />
+                <CreateCommentForm>
+                    <input type="text" placeholder='Write a comment...' />
+                    <div>
+                        <FaceOutlined />
+                        <EmojiEmotionsOutlined />
+                        <GifOutlined />
+                        <SupervisedUserCircleOutlined />
+                    </div>
+                </CreateCommentForm>
+            </CreateComment>
+            <CommentList>
+               {
+                allComments.map(comment => (
+                    <Comment>
+                        <Avatar src={comment.profilePicture} className='avatar' />
+                        <CommentDetails>
+                            <h6>{comment.userName}</h6>
+                            <p>{comment.text}</p>
+                        </CommentDetails>
+                    </Comment>
+                ))
+               }
+            </CommentList>
         </Container>
     )
 }
@@ -222,7 +255,6 @@ const Icon = styled.div`
     .share {
         transform: rotateY(180deg);
         }
-    }
 
     :hover {
         background-color: #525252b0;
@@ -241,5 +273,93 @@ const Icon = styled.div`
         .MuiSvgIcon-root {
             font-size: 20px;
         }
+    }
+`
+
+const CreateComment= styled.div`
+    display: flex;
+    align-items: center;
+    padding-left: 1em;
+    padding-right: 1em;
+    margin-top: 1em;
+
+    .avatar {
+        height: 35px;
+        width: 35px
+    }
+`
+
+const CreateCommentForm = styled.form`
+    display: flex;
+    background-color: #3A3B3C;
+    height: 39px;
+    border-radius: 20px;
+    flex: 1;
+    margin-left: 0.5em;
+
+    input {
+        flex: 1;
+        background-color: #3A3B3C;
+        border: none;
+        border-radius: 20px;
+        outline: none;
+        margin-right: 0.7em;
+        padding: 1em;
+        font-size: 16px;
+        color: #e4e6eb;
+    }
+
+    input::placeholder {
+        font-size: 14px;
+        color: #e4e6eb;
+    }
+
+    div {
+        display: flex;
+        align-items: center;
+        width: 106px;
+        justify-content: space-between;
+        margin-right: 1em;
+    }
+
+    .MuiSvgIcon-root {
+        font-size: 20px;
+        cursor: pointer;
+    }
+`
+
+const CommentList = styled.div`
+    padding: 16px;
+`
+
+const Comment = styled.div`
+    display: flex;
+    align-items: flex-start;
+    margin-bottom: 0.7em;
+
+    .avatar {
+        height: 35px;
+        width: 35px
+    }
+`
+
+const CommentDetails = styled.div`
+    background-color: #3A3B3C;
+    padding: 0.7em;
+    border-radius: 20px;
+    margin-left: 0.5em;
+
+    h6 {
+        font-weight: 600;
+        font-size: 13px;
+        margin-bottom: 5px;
+        color: #e4e6eb;
+    }
+
+    p {
+        color: #e4e6eb;
+        line-height: 20px;
+        font-size: 15px;
+        font-weight: 400;
     }
 `
--- src/components/ContentList.js
+++ src/components/ContentList.js
@@ -5,12 +5,48 @@ import Content from './Content'
 function ContentList() {
     return (
         <Container>
-            <Content />
-            <Content />
-            <Content />
-            <Content />
-            <Content />
-            <Content />
+            <Content 
+                userName='John doe'
+                profilePicture='https://images.unsplash.com/photo-1593990965215-075c1f918806?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8aGFuZHNvbWUlMjBibGFjayUyMG1hbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'
+                likes={10}
+                liked={true}
+                text='The all New Apple Macbook Pro M1 chip is Here Guys'
+                postImage="https://sb.nhattao.com/2018/12/11694856_macbook-pro-retina-15-inch-early-2015.jpg"
+                comments={[
+                    {
+                        userName: 'Ben carson',
+                        text: `Today my colleague said we won't ask the prof. About the duty and we won't do it to make him realize how much pressure he is putting us through, and then she did asked him and did the duty alone `,
+                        profilePicture: `https://media.istockphoto.com/photos/portrait-of-a-handsome-black-man-picture-id1289461328?b=1&k=20&m=1289461328&s=170667a&w=0&h=SpRhSvRMO7UkXWo52mV9bf0bo6au6kC-2wsOGsQ0D2Y='>
+                        <Profile src="https://media.istockphoto.com/photos/portrait-of-a-handsome-black-man-picture-id1289461328?b=1&k=20&m=1289461328&s=170667a&w=0&h=SpRhSvRMO7UkXWo52mV9bf0bo6au6kC-2wsOGsQ0D2Y=`
+                    },
+                    {
+                        userName: 'Ann Christine',
+                        text: `Well said`,
+                        profilePicture: `https://media.istockphoto.com/photos/success-happens-the-moment-you-believe-it-will-picture-id1262964438?b=1&k=20&m=1262964438&s=170667a&w=0&h=rgmdwXyMrmNuY_3BlmBFI0MdeayurqAjQSvweG4Htso=`
+                    }
+                ]}
+             />
+             <Content 
+                userName='Chris paul'
+                profilePicture='https://media.istockphoto.com/photos/cool-african-man-picture-id544358094?b=1&k=20&m=544358094&s=170667a&w=0&h=rM_-nTla_wODf5XRfNc2UuVrttjzGoF7fSD_gSjpHkM='
+                likes={10}
+                liked={true}
+                text='The all New Apple Macbook Pro M1 chip is Here Guys'
+                postImage="https://sb.nhattao.com/2018/12/11694856_macbook-pro-retina-15-inch-early-2015.jpg"
+                comments={[
+                    {
+                        userName: 'Ben carson',
+                        text: `Today my colleague said we won't ask the prof. About the duty and we won't do it to make him realize how much pressure he is putting us through, and then she did asked him and did the duty alone `,
+                        profilePicture: `https://media.istockphoto.com/photos/portrait-of-a-handsome-black-man-picture-id1289461328?b=1&k=20&m=1289461328&s=170667a&w=0&h=SpRhSvRMO7UkXWo52mV9bf0bo6au6kC-2wsOGsQ0D2Y='>
+                        <Profile src="https://media.istockphoto.com/photos/portrait-of-a-handsome-black-man-picture-id1289461328?b=1&k=20&m=1289461328&s=170667a&w=0&h=SpRhSvRMO7UkXWo52mV9bf0bo6au6kC-2wsOGsQ0D2Y=`
+                    },
+                    {
+                        userName: 'Ann Christine',
+                        text: `Well said`,
+                        profilePicture: `https://media.istockphoto.com/photos/success-happens-the-moment-you-believe-it-will-picture-id1262964438?b=1&k=20&m=1262964438&s=170667a&w=0&h=rgmdwXyMrmNuY_3BlmBFI0MdeayurqAjQSvweG4Htso=`
+                    }
+                ]}
+             />
         </Container>
             
     )
